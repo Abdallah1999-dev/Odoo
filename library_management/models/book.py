51a1de79a3602cba6fb763a171ce6a4abbd1bc87
saadany <abdallah@sijilacc.com>
2025-11-12 16:02:21 +0300

    feat(book): add validation for published year and fix tag color display

--- library_management/models/book.py
+++ library_management/models/book.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from odoo.exceptions import  ValidationError
+
 
 class Book(models.Model):
     _inherit =  ['mail.thread','mail.activity.mixin']
@@ -12,4 +14,15 @@ class Book(models.Model):
     category_id = fields.Many2many('library.book.tag')
     copies_total = fields.Integer(required=True)
     copies_available = fields.Integer()
-    published_date = fields.Date()
+    published_year = fields.Char(default="19xx")
+    description = fields.Text()
+
+
+    @api.constrains('published_year')
+    def check_published_year_val(self):
+        for rec in self:
+            year = rec.published_year
+            if not year.isdigit() or len(year) != 4:
+                raise ValidationError('Invalid Published Year! Please enter a 4-digit number.')
+            else:
+                pass
